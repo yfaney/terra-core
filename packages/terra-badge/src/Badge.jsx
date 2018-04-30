ec888545ffc9b1ab3967cff448d3346c664d8c40
Dylan Klohr <dylan.sftwr@gmail.com>
2018-04-29 21:35:03 -0500

    terra-badge prop enumeration

--- packages/terra-badge/src/Badge.jsx
+++ packages/terra-badge/src/Badge.jsx
@@ -6,6 +6,24 @@ import styles from './Badge.scss';
 
 const cx = classNames.bind(styles);
 
+const BadgeIntents = {
+  DEFAULT: 'default',
+  PRIMARY: 'primary',
+  SECONDARY: 'secondary',
+  INFO: 'info',
+  WARNING: 'warning',
+  POSITIVE: 'positive',
+  NEGATIVE: 'negative',
+};
+
+const BadgeSizes = {
+  TINY: 'tiny',
+  SMALL: 'small',
+  MEDIUM: 'medium',
+  LARGE: 'large',
+  HUGE: 'huge',
+};
+
 const propTypes = {
   /**
    * Child Nodes.
@@ -16,17 +34,17 @@ const propTypes = {
    */
   icon: PropTypes.element,
   /**
-   * Sets the badge color scheme. One of `default`, `primary`, `secondary`, `positive`, `negative`, `warning`, `info`.
+   * Sets the badge color scheme. One of `Badge.Opts.Intents.DEFAULT`, `Badge.Opts.Intents.PRIMARY`, `Badge.Opts.Intents.SECONDARY`, `Badge.Opts.Intents.POSITIVE`, `Badge.Opts.Intents.NEGATIVE`, `Badge.Opts.Intents.WARNING`, `Badge.Opts.Intents.INFO`.
    */
-  intent: PropTypes.oneOf(['default', 'primary', 'secondary', 'info', 'warning', 'positive', 'negative']),
+  intent: PropTypes.oneOf(Object.values(BadgeIntents)),
   /**
    * Reverses the position of the icon and text.
    */
   isReversed: PropTypes.bool,
   /**
-   * Sets the badge size. One of `tiny`, `small`, `medium`, `large`, `huge`.
+   * Sets the badge size. One of `Badge.Opts.Sizes.TINY`, `Badge.Opts.Sizes.SMALL`, `Badge.Opts.Sizes.MEDIUM`, `Badge.Opts.Sizes.LARGE`, `Badge.Opts.Sizes.HUGE`.
    */
-  size: PropTypes.oneOf(['tiny', 'small', 'medium', 'large', 'huge']),
+  size: PropTypes.oneOf(Object.values(BadgeSizes)),
   /**
    * Sets the badge text.
    */
@@ -61,7 +79,13 @@ const Badge = ({
   return React.createElement('span', { ...customProps, className: badgeClassNames }, ...content);
 };
 
+const opts = {
+  Intents: BadgeIntents,
+  Sizes: BadgeSizes,
+};
+
 Badge.propTypes = propTypes;
 Badge.defaultProps = defaultProps;
+Badge.Opts = opts;
 
 export default Badge;
