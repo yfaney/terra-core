1998ef4a7aabfaec1fcf5777d92491634e14b8db
Dylan Klohr <dylan.sftwr@gmail.com>
2018-05-01 08:35:01 -0500

    removed all references to Card.Opts and added readme example

--- packages/terra-card/src/Card.jsx
+++ packages/terra-card/src/Card.jsx
@@ -6,11 +6,11 @@ import styles from './Card.scss';
 import CardBody from './CardBody';
 
 const cx = classNames.bind(styles);
+
 const CardVariants = {
   DEFAULT: 'default',
   RAISED: 'raised',
 };
-
 
 const propTypes = {
   /**
@@ -18,9 +18,9 @@ const propTypes = {
    */
   children: PropTypes.node,
   /**
-   * Sets the card variant to change the style for different use cases. One of `Card.Opts.Variants.DEFAULT`,  `Card.Opts.Variants.RAISED`.
+   * Sets the card variant to change the style for different use cases. One of `default`,  `raised`.
    */
-  variant: PropTypes.oneOf(Object.values(CardVariants)),
+  variant: PropTypes.oneOf([...Object.values(CardVariants)]),
 };
 
 const defaultProps = {
@@ -41,12 +41,8 @@ const Card = ({
   return <div {...customProps} className={cardClassNames}>{children}</div>;
 };
 
-const opts = {
-  Variants: CardVariants,
-};
-
 Card.propTypes = propTypes;
 Card.defaultProps = defaultProps;
 Card.Body = CardBody;
-Card.Opts = opts;
+
 export default Card;
