f85ae63855ef21f7109db26fbbf9b09509daf23e
Brett Jankord <brett.jankord@cerner.com>
2018-06-01 05:02:23 -0500

    Wip

--- packages/terra-heading/src/terra-dev-site/doc/example/HeadingColors.jsx
+++ packages/terra-heading/src/terra-dev-site/doc/example/HeadingColors.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import Heading from 'terra-heading/lib/Heading';
 
 const HeadingColors = () => (
